b62331776bc3226ce8d0e960965339999f040b7a
padillaguadalupe <padillaguadalupe.epet20@gmail.com>
2023-04-19 09:25:10 -0300

    bajando archivos package-lock.json

--- proyecto-muni-nqn/src/App.js
+++ proyecto-muni-nqn/src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+import { useEffect, useState } from 'react';
+
+const Test = () => {
+    const [form, setForm] = useState({
+        uno: '',
+        dos: '',
+        tres: '',
+    });
+
+    const cambiarUno = (e) => {
+        const value = e.target.value;
+
+        setForm({
+            ...form,
+            uno: value,
+        });
+    };
+
+    const cambiarDos = (e) => {
+        const value = e.target.value;
+
+        setForm({
+            ...form,
+            dos: value,
+        });
+    };
+
+    const cambiarTres = (e) => {
+        const value = e.target.value;
+
+        setForm({
+            ...form,
+            tres: value,
+            otro: null,
+        });
+    };
+
+    const enviarInformacion = () => {
+        if (form.dos.length === 8) {
+            console.log('enviado');
+        }
+    };
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <input
+                        type="text"
+                        placeholder="Nombre"
+                        value={form.uno}
+                        onChange={cambiarUno}
+                    />
+                    <input
+                        type="text"
+                        placeholder= "Apellido" 
+                        onkeypress= "return (event.charCode >= 48 && event.charCode <= 57)"
+                        value={form.dos}
+                        onChange={cambiarDos}
+                      
+                    />
+
+                    <input
+                        type="text"
+                        placeholder="DNI"
+                        value={form.tres}
+                        onChange={cambiarTres}
+                        pattern="[0-9]" maxlength="8"
+                        
+                    />
+
+                    <button onClick={enviarInformacion}>Enviar</button>
+                </div>
+            </div>
+        </>
+    );
+};
+export default Test;
