92e70dcd40bad954584cee37c76b7a4af70b120b
Angel Luciano Ciancio <ciancioangel.epet20@gmail.com>
2023-04-19 01:11:16 -0300

    Implementando formulario test

--- proyecto-muni-nqn/src/App.js
+++ proyecto-muni-nqn/src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Test = () => {
+    const [form, setForm] = useState({
+        nombre: '',
+        apellido: '',
+        edad: '',        
+        genero: '',
+        dni: '',
+        modalidad: '',
+    });
 
-export default App;
+    const cambiarNombre = (e) => {
+        const value = e.target.value;
+        const regex = /^[a-zA-ZÁÉÍÓÚáéíóúñÑ\s\-\']+$/; // Expresión regular para permitir solo letras y algunos caracteres especiales
+        //regex permite solo números y caracteres de retroceso (\b).
+        if (value === "" || regex.test(value)) {
+        //verificamos si el valor ingresado coincide con la expresión regular y si es así, se actualiza el valor de setForm.
+          setForm({
+              ...form,
+              nombre: value,
+          });
+        }
+    };
+
+    const cambiarApellido = (e) => {
+        const value = e.target.value;
+        const regex = /^[a-zA-ZÁÉÍÓÚáéíóúñÑ\s\-\']+$/; // Expresión regular para permitir solo letras y algunos caracteres especiales
+        //regex permite solo números y caracteres de retroceso (\b).
+        if (value === "" || regex.test(value)) {
+        //verificamos si el valor ingresado coincide con la expresión regular y si es así, se actualiza el valor de setForm.
+          setForm({
+              ...form,
+              apellido: value,
+          });
+        }
+
+    };
+
+    const cambiarEdad = (e) => {
+        const value = e.target.value;
+        const regex = /^[0-9\b]+$/; 
+        //regex permite solo números y caracteres de retroceso (\b).
+        if (value === "" || regex.test(value)) {
+        //verificamos si el valor ingresado coincide con la expresión regular y si es así, se actualiza el valor de setForm.
+          setForm({
+              ...form,
+              edad: value,
+          });
+        }
+    };
+    const cambiarGenero = (e) => {
+      const value = e.target.value;
+
+      setForm({
+          ...form,
+          genero: value,
+      });
+  };
+
+  const cambiarDni = (e) => {
+      const value = e.target.value;
+      const regex = /^[0-9\b]+$/;
+      if (value === "" || regex.test(value)) {
+        setForm({
+            ...form,
+            dni: value,
+        });
+      }
+  };
+
+  const cambiarModalidad = (e) => {
+      const value = e.target.value;
+
+      setForm({
+          ...form,
+          modalidad: value,
+      });
+  };
+    const enviarInformacion = () => {
+        if ((form.dni.length === 8) & (form.edad.length ===3)){
+            console.log('enviado');
+            console.log(form)
+        }
+    };
+    return (
+        <>
+          <div className="container">
+            <div className="form">
+              <p class="title">Registro</p>
+              <div class="form-row">
+                <label class="name">
+                  <span>Nombre</span>
+                  <input class="input" type="text" value={form.nombre} onChange={cambiarNombre} />
+                </label>
+                <label class="apellido">
+                  <span>Apellido</span>
+                  <input class="input" type="text" value={form.apellido} onChange={cambiarApellido} />
+                </label>
+              </div>
+              <div class="form-row">
+                <label class="edad">
+                  <span>Edad</span>
+                  <input class="input" type="text" value={form.edad} onChange={cambiarEdad} maxLength={3} />
+                </label>
+                <label class="dni">
+                  <span>DNI</span>
+                  <input class="input" type="text" value={form.dni} onChange={cambiarDni} maxLength={8} />
+                </label>
+              </div>
+              <div class="form-row">
+              <label class="genero">
+                <span>Genero</span>
+                <div class="select-container">
+                  <select class="input" value={form.genero} onChange={cambiarGenero}>
+                    <option value="" disabled selected>Genero</option>
+                    <option value="Hombre">Hombre</option>
+                    <option value="Mujer">Mujer</option>
+                  </select>
+                </div>
+              </label>
+              <label class="modalidad">
+                <span>Modalidad</span>
+                <div class="select-container">
+                  <select class="input" value={form.modalidad} onChange={cambiarModalidad}>
+                    <option value="" disabled selected>Modalidad</option>
+                    <option value="Grupal">Grupal</option>
+                    <option value="Individual">Individual</option>
+                  </select>
+                </div>
+              </label>
+              </div>
+              <button id="btn" onClick={enviarInformacion}>Enviar informacion</button>
+            </div>
+          </div>
+
+        </>
+    );
+};
+
+export default Test;
