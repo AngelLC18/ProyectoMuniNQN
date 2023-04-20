5729e4df30ae3e3be96778735e45199fcba78bf0
AngelLC18 <ciancioangel.epet20@gmail.com>
2023-04-20 12:45:58 -0300

    Enrutando pruebas al index.js

--- proyecto-muni-nqn/src/index.js
+++ proyecto-muni-nqn/src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Pruebas from './pruebas'
 import reportWebVitals from './reportWebVitals';
-import 'boostrap/dist/css/boostrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Pruebas />
   </React.StrictMode>
 );
 
