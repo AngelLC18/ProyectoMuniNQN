bd84c39638af13508a14babc24a8207db1c1377c
Angel Luciano Ciancio <ciancioangel.epet20@gmail.com>
2023-04-21 07:13:40 -0300

    Agregando estructura de peticiones a la api

--- proyecto-muni-nqn/src/components/ShowInfo.js
+++ proyecto-muni-nqn/src/components/ShowInfo.js
@@ -1,43 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios  from 'axios';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
-import { show_alert } from '../functions';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "react-bootstrap";
 
+const PersonTable = () => {
+  const [people, setPeople] = useState([]);
 
-const ShowInfo = () => {
-    const [id,setId] = useState ([]);
+  useEffect(() => {
+    axios
+      .get("https://weblogin.muninqn.gov.ar/api/Examen/personas")
+      .then((response) => {
+        const uniquePeople = Array.from(
+          new Set(response.data.map((person) => person.DNI))
+        ).map((dni) =>
+          response.data.find((person) => person.DNI === dni)
+        );
+        setPeople(uniquePeople);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    //const [id,setId] = useState('');
-    //const [name,setName] = useState ('');
-    //const [surname,setSurname] = useState ('');
-    //const [age,setAge] = useState ('');
-    //const [dni,setDni] = useState ('');
-    //const [gender,setGender] = useState ('');
-    //const [modality,setModality] = useState ('');
-    //const [title,setTitle] = useState ('');
-    //const [operation,setOperation] = useState ('1');
-
-    useEffect ( () =>{
-        axios.get('https://weblogin.muninqn.gov.ar/api/Examen')
-        .then(response => {
-            setId(response.data);
-        })
-        .catch(error =>{
-            console.log(error);
-        })
-    },[]);
-    //console.log(personas.length)
-    //if(personas.length === 0)
-    //return <h1>No hay personas</h1>
-
-    return (
-        <div className='App'>
-            <ul>
-
-            </ul>
-        </div>
-    )
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>DNI</th>
+          <th>Género</th>
+          <th>Edad</th>
+        </tr>
+      </thead>
+      <tbody>
+        {people.map((person) => (
+          <tr key={person.DNI}>
+            <td>{person.nombre}</td>
+            <td>{person.apellido}</td>
+            <td>{person.DNI}</td>
+            <td>{person.genero}</td>
+            <td>{person.edad}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 }
 
-export default ShowInfo
+export default PersonTable;
