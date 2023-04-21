7dd9cc5e860cda1f0335e272869165b5d017f485
padillaguadalupe <padillaguadalupe.epet20@gmail.com>
2023-04-21 10:52:13 -0300

    Agregando tablas MySQL

---
+++ backend/proyectoMunibd.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE proyectoMuniNQN;
+USE proyectoMuniNQN;
+
+CREATE TABLE personas (
+  id INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  dni INT NOT NULL,
+  genero ENUM('Masculino', 'Femenino') NOT NULL,
+  edad INT NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (dni)
+);
+CREATE TABLE cursos (
+  id INT NOT NULL AUTO_INCREMENT,
+  nombreU VARCHAR(50) NOT NULL,
+  legajo VARCHAR(50) NOT NULL,
+  descripcion TEXT(100),
+  id_Modalidad INT,
+  PRIMARY KEY (id),
+  UNIQUE KEY (legajo)
+);
+CREATE TABLE PersonasxCurso (
+  id_Personas INT,
+  id_Cursos INT,
+  PRIMARY KEY (id_Personas,id_Cursos)
+);
+CREATE TABLE Modalidad (
+  id INT,
+  Descripcion INT,
+  PRIMARY KEY (id),
+  UNIQUE KEY (Descripcion)
+);
+
